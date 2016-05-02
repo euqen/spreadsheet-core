ff38a6193b9c8717097887ed27182f9503ddd823
vitalyorlov <vitali.orlov.96@gmail.com>
2016-05-02 14:45:26 +0300

    [+] Add password generation and password sendmail function in registration space

--- server/src/resources/user/user.controller.js
+++ server/src/resources/user/user.controller.js
@@ -3,6 +3,9 @@ import removeValidator from './validators/user.remove.validator';
 import updateValidator from './validators/user.update.validator';
 import service from './user.service';
 import languages from './../../internal/languages';
+import nodemailer from 'nodemailer';
+import generatePassword from "password-generator";
+import authService from './../auth/auth.service';
 
 export default {
     create(req, res) {
@@ -11,7 +14,12 @@ export default {
                 if (!data.isValid) {
                    return;
                 }
+
+                let password = authService.generateSecurePassword();
+                this.sendPasswordInMail(data.result.email, password);
+
                 data.result.locale = languages.en;
+                data.result.password = authService.generateHash(password);
                 return service.create(data.result);
             });
     },
@@ -56,7 +64,7 @@ export default {
                 if (!data.isValid) {
                     return;
                 }
-                
+
                 return service.updateOne({_id: req.user._id}, doc => {
                     doc.firstName = data.firstName;
                     doc.middleName = data.middleName;
@@ -66,6 +74,31 @@ export default {
                     }
                 });
             });
+    },
+
+    sendPasswordInMail(to, password) {
+        var smtpTransport = nodemailer.createTransport("SMTP",{
+            service: "Gmail",
+            auth: {
+                user: "bsuir2017@gmail.com",
+                pass: "bsuir12345"
+            }
+        });
+
+        smtpTransport.sendMail({
+            from: "bsuir2017@gmail.com",
+            to: to,
+            subject: "Your password in spreadsheet system",
+            text: "Your password: " + password
+        }, function(error, response){
+            if(error){
+                console.log(error);
+            }else{
+                console.log("Message sent: " + response.message);
+            }
+
+            smtpTransport.close();
+        });
     }
-    
+
 }
